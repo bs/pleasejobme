c8105923fa071fa55482b843ea38912df21a3213
Patrick Ewing <patrick@twitter.com>
2010-03-09 13:15:36 -0800

    Embed tweet templates in pin popups, better styling, decompose the JS a little bit.

--- javascripts/sxsw.js
+++ javascripts/sxsw.js
@@ -1,15 +1,53 @@
-function initialize() {
-  var latlng = new google.maps.LatLng(37, -122);
-  var myOptions = {
-    zoom: 8,
-    center: latlng,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
+/*jslint white: true, indent: 2, browser: true,  */
+/*global google, map, Mustache, $ */
+//
+// var tweepsToMap = [
+//   'pandemona',
+//   'hoverbird',
+//   'bs',
+//   'k',
+// ]
+
+var tweetUserTemplate =
+  '<div class="tweetUserPopup">' +
+    '<a href="http://twitter.com/{{screen_name}}" class="profile-pic"><img src="{{avatar_src}}" width="48" height="48"></a>&nbsp;' +
+    '<a href="http://twitter.com/{{screen_name}}">@{{screen_name}}&#8212; <small>{{full_name}}</small></a>' +
+    '<span class="tweet-text">{{text}}</span>' +
+  '</div>';
+
+function tweetViewFor(tweet) {
+  var user = tweet.user;
+  var view = {
+    screen_name : user.screen_name,
+    full_name   : user.name,
+    avatar_src : user.profile_image_url,
+    text : tweet.text
   };
-  map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
-  getStuffFromLists()
+  return view;
 }
 
-function getStuffFromLists() {
+function plotTweets(results) {
+  $.each(results, function () {
+    if (this.geo !== null) {
+      var infowindow = new google.maps.InfoWindow({
+        content: Mustache.to_html(tweetUserTemplate, tweetViewFor(this))
+      });
+
+      var marker = new google.maps.Marker({
+        position: new google.maps.LatLng(this.geo.coordinates[0], this.geo.coordinates[1]),
+        title: this.user.screen_name,
+        map: map
+      });
+
+      google.maps.event.addListener(marker, 'click', function () {
+        infowindow.open(map, marker);
+      });
+
+    }
+  });
+}
+
+function getTweetsFromLists() {
   $.ajax({
     url: "http://api.twitter.com/1/twitter/lists/team/statuses.json?per_page=200",
     dataType: 'jsonp',
@@ -19,36 +57,13 @@ function getStuffFromLists() {
   });
 }
 
-var tweetUserTemplate = 
-  '<div class="tweetUserPopup"><a href="http://twitter.com/{{screen_name}}">@{{screen_name}}</a>' +
-    '{{text}}' +
-  '</div>'
-  
-function tweetViewFor(tweet) {
-  return {
-    screen_name : tweet.user.screen_name,
-    text : tweet.text
-  }
-}
-
-function plotTweets(results) {
-  $.each(results, function() { 
-    if (this.geo != null) {
-      var view = tweetViewFor(this);
-      var tweetUserPopup = Mustache.to_html(tweetUserTemplate, view);
-      
-      var infowindow = new google.maps.InfoWindow({ content: tweetUserPopup });
-      var latlng = new google.maps.LatLng(this.geo.coordinates[0], this.geo.coordinates[1]);
-      var marker = new google.maps.Marker({
-        title: view.screen_name,
-        position: latlng, 
-        map: map
-      });
-      
-      google.maps.event.addListener(marker, 'click', function() {
-        infowindow.open(map, marker);
-      });
-     
-    }
-  });
+function initialize() {
+  var latlng = new google.maps.LatLng(37, -122);
+  var myOptions = {
+    zoom: 8,
+    center: latlng,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+  getTweetsFromLists();
 }
