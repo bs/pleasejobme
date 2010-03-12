8cb6da694e6f4c41422803ddf41017528c50ec8e
Patrick Ewing <patrick@twitter.com>
2010-03-12 11:57:01 -0600

    Questionnairre is fully functional now. Still needs desing love, though.

--- javascripts/questionnaire.js
+++ javascripts/questionnaire.js
@@ -12,41 +12,37 @@ var questionnaireSelectTemplate =
     '<input type="checkbox" id="select_{{id}}" name="questionnaire[{{id}}]" value="" style="display:none;"/>' +
   '</li>';
 
-function elementUnselected(listElement) {
-   listElement.find('input[type="checkbox"]').attr("value", "");
-   listElement.removeClass('selected');
-   listElement.click(function () {
-     elementSelected(listElement);
-   });
-}
-
-function removePerson(id, skill) {
-  var personElement = $('#person_' + id );
-  var newArray = $.grep(twitterPeople[id].requestedInterests, function(value) {
-    return value !== removeItem;
-  });
-  console.log(twitterPeople[id]);
-  twitterPeople[id].requestedInterests = newArray;
-  if (newArray.length = 0) {
-    personElement.hide();
-  }
-}
-
 function addPerson(id, skill) {
   var personElement = $('#person_' + id );
   var person = twitterPeople[id];
-  if (person == null) {
-    console.log("Couldn't find a twitterPerson with ID " + id)
+  if (person === null) {
+    console.log("Couldn't find a twitterPerson with ID " + id);
   } else {
-    console.log(person);
     person.requestedInterests.push(skill);
     personElement.show();
   }
 }
 
-function hidePerson(id) {
-  twitterPeople[id].requestedInterests.push(skill);
-  $('#person_' + id ).hide();
+function removePerson(id, skill) {
+  var personElement = $('#person_' + id );
+  var filteredInterests = $.grep(twitterPeople[id].requestedInterests, function(value, index) {
+    return value != skill;
+  });
+  if (filteredInterests.length <= 0) {
+    personElement.hide();
+  }
+  twitterPeople[id].requestedInterests = filteredInterests;
+}
+
+function elementUnselected(listElement) {
+   $.each(listElement.data('people'), function () {
+     removePerson(this, listElement.attr('id'));
+   });
+   listElement.find('input[type="checkbox"]').attr("value", "");
+   listElement.removeClass('selected');
+   listElement.one('click', function () {
+     elementSelected(listElement);
+   });
 }
 
 function elementSelected(listElement) {
@@ -56,34 +52,33 @@ function elementSelected(listElement) {
     addPerson(this, listElement.attr('id'));
   });
 
-  listElement.click(function () {
+  listElement.one('click', function () {
     elementUnselected(listElement);
   });
 }
 
-function drawPeople() {
-  $.each(twitterPeople, function(id, personObject) {
+function drawPeople(people) {
+  $.each(people, function(id, personObject) {
     var twitterPersonObject = $.extend(personObject, {'id' : id});
     var personElement = $(Mustache.to_html(twitterPersonTemplate, twitterPersonObject));
-
     $('body').append(personElement);
   });
 }
 
 function initializeQuestionnaire () {
   var formSelections = $("form#questionnaire ul");
-  drawPeople();
 
   $.each(questionSelections, function () {
     formSelections.append($("<h3>" + this[0] + "</h3>"));
     $.each(this[1], function () {
       var listElement = $(Mustache.to_html(questionnaireSelectTemplate, this));
       listElement.data('people', this.people);
-
-      listElement.click(function () {
+      listElement.one('click', function () {
         elementSelected(listElement);
       });
       formSelections.append(listElement);
     });
   });
+
+  drawPeople(twitterPeople);
 }
